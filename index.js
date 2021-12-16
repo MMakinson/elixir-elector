a3f9f8f449d9d079b4425f0e558df9271fcfbeff
MMakinson <mmakinson80@gmail.com>
2021-12-15 18:04:48 -0800

    cleaned up logic, added base url variable and error message

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/'
+
 const getDrinkName = (event) => {
     event.preventDefault()
     const query = document.querySelector("#drinkName").value
     console.log("query", query)
     if (query === "random") {
         randomDrink()
+    } else if (query.length === 1) {
+        drinkByLetter(query)
     } else {
-        console.log('not random')
         getDrinks(query)
     }
 }
     
-const searchS = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const searchF = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='
-const getDrinks = async (query) => {
-    if(query.length === 1){
-        const response = await axios.get(`${searchF}${query}`);
-        console.log("response.data.drinks searchF", response.data.drinks);
-    }else if (query) {
-        const response = await axios.get(`${searchS}${query}`);
-        console.log("response.data.drinks searchS", response.data.drinks);
-    }else {
-        console.log("whoops, better check your spelling and try again. Heres a random drink on the house.")
-        randomDrink()
-    }
+const drinkByLetter = async (query) => {
+    const response = await axios.get(`${baseUrl}search.php?f=${query}`);
+    console.log("response.data.drinks by letter", response.data.drinks);
 }
 
+const getDrinks = async (query) => {
+        const response = await axios.get(`${baseUrl}search.php?s=${query}`);
+        if (response.data.drinks !== null) {
+            console.log("getDrinks response is not null", response.data.drinks)
+        } else {
+            console.log("whoops, better check your spelling and try again. Heres a random drink on the house.")
+            randomDrink()
+        }
+    }
+
+
 const randomDrink = async () => {
-    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
+    const response = await axios.get(`${baseUrl}random.php`);
     console.log("response.data.drinks", response.data.drinks);
 }
 
-// const getAllDrinksByLetter = async (query) => {
-//     const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${query}`);
-//     console.log("response.data.drinks", response.data.drinks);
-// }
 
-// const getLiquor = async (query) => {
-//     const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${query}`);
-//     console.log("response.data.drinks", response.data);
-// }
 // stretch goal: allow serach by alcohol type (would need to search each drink object for ingredient of liquor type)
 
-
-
-
-// getDrinks();
-// submitBtn.addEventListener('click', () => {
-//     console.log('it clicked')
-// })
-// const textArea = document.getElementById("drinkName").addEventListener('blur', () => {
-//     let inputVal = textArea.value;
-//     console.log("inputVal", inputVal )
-// })
