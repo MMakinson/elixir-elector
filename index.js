b307d797fd6e4fb35055652819264f3bbf65ea50
MMakinson <mmakinson80@gmail.com>
2021-12-22 09:03:54 -0800

    reworked project to simplify to random drink, set up all DOM manipulation, begin styling of drink display

--- index.js
+++ index.js
@@ -1,77 +1,152 @@
 const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/'
-const drinkResult3 = document.getElementById('drinkResultGrid3');
-const drinkResult4 = document.getElementById('drinkResultGrid4');
-const drinkResult5 = document.getElementById('drinkResultGrid5');
-const imgResult = document.getElementById('imgDisplay');
 
-const getDrinkName = (event) => {
-    // prevent html form from reloading the whole page as a default bx
-    event.preventDefault()
-    const query = document.querySelector("#drinkName").value
-    // what is the query value here? It returns as completely blank if I hit search without entering anything into the input field
-    console.log("query value:", query)
-    if (query === "random") {
-        randomDrink()
-    } else if (query.length === 1) {
-        drinkByLetter(query)
-    } else {
-        getDrinks(query)
-    }
-}
+
+
+const button = document.querySelector('#btn')
+button.addEventListener('click', () => {
+    randomDrink()
+})
     
-const drinkByLetter = async (query) => {
-    const response = await axios.get(`${baseUrl}search.php?f=${query}`);
-    console.log("response.data.drinks by letter", response.data.drinks);
-    // how can I loop over the response and pull out the data that I want for each drink?
-    for (let drinks in response) {
-        console.log(`${response[drinks].strDrink}`)
-    }
-}
-
-const getDrinks = async (query) => {
-        const response = await axios.get(`${baseUrl}search.php?s=${query}`);
-        if (response.data.drinks !== null) {
-            // how can I loop over the response and pull out the data that I want for each drink?
-            console.log("getDrinks response is not null", response.data.drinks)
-        } else {
-            console.log("whoops, better check your spelling and try again. Heres a random drink on the house.")
-            randomDrink()
-        }
-    }
-
 
 const randomDrink = async () => {
     const response = await axios.get(`${baseUrl}random.php`);
-    // how can I make this cleaner/less lines of code?
-    const drinkName = response.data.drinks[0].strDrink
-    const drinkType= response.data.drinks[0].strAlcoholic
-    const drinkImg = response.data.drinks[0].strDrinkThumb
-    const glassType = response.data.drinks[0].strGlass
-    // if there is no drink information then display nothing rather than null
-    const ingred1 = response.data.drinks[0].strIngredient1
-    const ingred2 = response.data.drinks[0].strIngredient2
-    const ingred3 = response.data.drinks[0].strIngredient3
-    const ingred4 = response.data.drinks[0].strIngredient4
-    const ingred5 = response.data.drinks[0].strIngredient5
-    const instructions = response.data.drinks[0].strInstructions
-    // how can this part be simplified/built dynamically using a loop?
-    console.log("response.data.drinks", response.data.drinks);
-    drinkResult3.innerHTML = `<h2>${drinkName}</h2><p>${drinkType}</p><p>${glassType}</p>`
-    drinkResult4.innerHTML = `
-    <p>${ingred1}</p>
-    <p>${ingred2}</p>
-    <p>${ingred3}</p>
-    <p>${ingred4}</p>
-    <p>${ingred5}</p>`
-    drinkResult5.innerHTML= `<div class="grid5"><p>${instructions}</p></div>`
-    imgResult.innerHTML = `<img src=${drinkImg}>`
+    let results = response.data.drinks;
+    console.log(results)
+    results.forEach((result) => {
+        
+        // img display grid (grid #2)
+        let showImg = document.createElement('img')
+        document.getElementById('imgDisplay').appendChild(showImg)
+        showImg.setAttribute('src', result.strDrinkThumb)
+        
+        let resultDiv = document.createElement('div')
+        
+        // create h2 for drink title and add text for title, add it to the drinkResultGrid3
+        let drinkName = document.createElement("h2")
+        drinkName.innerText = result.strDrink
+        document.getElementById('drinkResultGrid2').appendChild(drinkName);
+        console.log('what is drinkName', drinkName)
+
+
+        // grab the UL element and append the li's to it for ingredients
+        let ingredList = document.createElement('ul')
+        document.getElementById('drinkResultGrid2').appendChild(ingredList)
+        console.log('what is ingredlist', ingredList)
+
+            if(result.strIngredient1) {
+                let ingred1 = document.createElement('li')
+                ingred1.innerText = result.strIngredient1
+                ingredList.append(ingred1)
+            }
+            if(result.strIngredient2) {
+                let ingred2 = document.createElement('li')
+                ingred2.innerText = result.strIngredient2
+                ingredList.append(ingred2)
+            }
+            if(result.strIngredient3) {
+                let ingred3 = document.createElement('li')
+                ingred3.innerText = result.strIngredient3
+                ingredList.append(ingred3)
+            }
+            if(result.strIngredient4) {
+                let ingred4 = document.createElement('li')
+                ingred4.innerText = result.strIngredient4
+                ingredList.append(ingred4)
+            }
+            if(result.strIngredient5) {
+                let ingred5 = document.createElement('li')
+                ingred5.innerText = result.strIngredient5
+                ingredList.append(ingred5)
+            }
+            if(result.strIngredient6) {
+                let ingred6 = document.createElement('li')
+                ingred6.innerText = result.strIngredient6
+                ingredList.append(ingred6)
+            }
+            if(result.strIngredient7) {
+                let ingred7 = document.createElement('li')
+                ingred7.innerText = result.strIngredient7
+                ingredList.append(ingred7)
+            }
+            if(result.strIngredient8) {
+                let ingred8 = document.createElement('li')
+                ingred8.innerText = result.strIngredient8
+                ingredList.append(ingred8)
+            }
+            if(result.strIngredient9) {
+                let ingred9 = document.createElement('li')
+                ingred9.innerText = result.strIngredient9
+                ingredList.append(ingred9)
+            }
+            
+            // create p tag to hold instruction info, put retrieved instruction data as text in p tag, append it to the drinkResultGrid4 div
+            let drinkInst = document.createElement('p')
+            drinkInst.innerText = result.strInstructions
+            document.getElementById('drinkResultGrid3').appendChild(drinkInst)
+            console.log('what is drinkInst', drinkInst)
+    })
 }
 
+
+
+// get the user-inputted value from the text input field
+// const getInputValue = (event) => {
+//     // prevent html form from reloading the whole page as a default bx
+//     event.preventDefault()
+//     // pass the value of the input to the variable query
+//     const query = document.querySelector("#drinkName").value
+//     if (query === "random") {
+//         randomDrink()
+//     } else if (query.length === 1) {
+//         drinkByLetter(query)
+//     } else if (query === "") {
+//         whoops();
+//     } else {
+//         getDrinks(query)
+//     }
+// }
+
+
+// const drinkByLetter = async (query) => {
+//     const response = await axios.get(`${baseUrl}search.php?f=${query}`);
+//     let results = response.data.drinks;
+//     console.log("response.data.drinks by letter", response.data.drinks);
+//     // how can I loop over the response and pull out the data that I want for each drink?
+//     results.forEach((result) => {
+//         let drinkName = document.createElement("li")
+//         let drinkList = document.getElementById('drinkList')
+//         drinkName.innerText = result.strDrink
+//         drinkList.append(drinkName)
+//     })
+// }
+
+// const getDrinks = async (query) => {
+//         const response = await axios.get(`${baseUrl}search.php?s=${query}`);
+//         let results = response.data.drinks;
+//         if (response.data.drinks !== null) {
+//             // how can I loop over the response and pull out the data that I want for each drink?
+//             console.log("getDrinks response is not null", response.data.drinks)
+//             results.forEach((result) => {
+//                 let drinkName = document.createElement("li")
+//                 let drinkList = document.getElementById('drinkList')
+//                 drinkName.innerText = result.strDrink
+//                 drinkList.append(drinkName)
+//             })
+//         } else {
+//             whoops();
+//         }
+//     }
     
-
-
-// stretch goal: allow serach by alcohol type (would need to search each drink object for ingredient of liquor type)
-
+//     const whoops = () => {
+//         let error = document.createElement("p")
+//         let errorDiv = document.getElementById('errorDiv')
+//         error.innerText = "whoops! Better check your spelling and try again. Heres a random drink on the house."
+//         errorDiv.append(error)
+//         randomDrink()
+//     }
+    
+    
+   
 
 
 
